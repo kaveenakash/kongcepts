d2bc0aba50ea866c146f6f5d3081fd8e09ebc843
kaveenakashya <kaveenakashya@gmail.com>
2021-08-27 11:29:37 +0530

    base setup

---
+++ Desktop/Social_Voice/socialui/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+
+function App() {
+  return (
+    <React.Fragment>
+        <Typography variant="h5">Base Setup Test</Typography>
+    </React.Fragment>
+  );
+}
+
+export default App;
